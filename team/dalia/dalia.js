a34903036091c1da93e0bd6b454d36bfea1cc976
DaliaAbdelghani <engdna87@gmail.com>
2021-02-28 22:30:19 +0200

    update

--- team/dalia/dalia.js
+++ team/dalia/dalia.js
@@ -24,13 +24,13 @@ Quiz.prototype.render = function () {
 Quiz.prototype.checkAnswer = function () {
   for (let i = 0; i < this.answers.length; i++) {
     if ( answer.value.toLowerCase().trim() === this.answers[i]) {
-      message.innerHTML = 'Correct';
+      message.innerHTML = 'EASY PEASY LEMON SQUEASY... NEXT QUESTION!';
       currentQ++;
       loadQuestion();
       break;
     }
     else {
-      message.innerHTML = 'Try again';
+      message.innerHTML = 'Think outside the box, baleez.';
     }
   }
 };
@@ -38,7 +38,7 @@ Quiz.prototype.checkAnswer = function () {
 
 const question1 = new Quiz('What is the number of the parking spot covered by the car?', ['87'], 'dalia.img/q1.PNG','https://media3.giphy.com/media/18BKYnQ5Kc2re/giphy.gif');
 const question2 = new Quiz('In a two-sides street there are six houses red, blue ,white, green, yellow and pink . Three are in a row at each side. The red house is right of the middle and opposite to the green house. The blue house is left of the middle and opposite to the pink house.The green house is next to the yellow house. Where is the White House?'
-  , ['in washington dc','washington', 'USA', 'united states'],'dalia.img/background2.gif' ,'dalia.img/background2.jpg');
+  , ['in washington dc','washington', 'USA', 'united states'],'dalia.img/background2.gif' ,'https://i1.wp.com/gonitsora.com/wp-content/uploads/2012/06/answer-bing-quizzes.jpg?fit=620%2C413&ssl=1');
 const question3 = new Quiz('In which direction the car is moving?', ['car is not moving, it has no wheels','car is not moving', 'not moving', 'stopped', 'no wheels'], 'dalia.img/q3.PNG','https://www.pwc.ie/images/2019/opposite-arrows-icon.gif');
 
 const questions = [question1, question2, question3];
