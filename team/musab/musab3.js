0c86ec5e24fb61610846c44ca1593117ced651ca
Musab-Al-omari <musabsomari@gmail.com>
2021-03-02 20:41:03 +0200

    add final box

--- team/musab/musab3.js
+++ team/musab/musab3.js
@@ -89,7 +89,7 @@ function newYolor() {
 
 
 
-
+// the timer;
 
 const TIME_LIMIT = 1200;
 let timePassed = 0;
@@ -114,9 +114,17 @@ function timerExp() {
 
   if (timeLeft === 0) {
     onTimesUp();
-  } else if (timeLeft===1170) {
+  } else if (timeLeft === 1195) {
+    let tryAgain = document.getElementById('alert-button');
+    document.getElementById('alert').style.visibility = 'visible';
+    document.getElementById('alert-text').innerHTML = 'the game is done ';
+    tryAgain.onclick = function () {
+      document.getElementById('alert').style.visibility = 'hidden';
 
-    window.location = '../../index.html';}
+      window.location = '../../index.html';
+    };
+
+  }
 }
 
 let body = document.querySelector('body');
@@ -141,59 +149,3 @@ function formatTime(time) {
 
 
 
-
-
-
-// const TIME_LIMIT = 120;
-// let timePassed = 0;
-// let timeLeft = TIME_LIMIT;
-// let timerInterval = null;
-
-
-// document.getElementById('app').innerHTML = `
-// <div class="base-timer">
-//   <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-//     <g class="base-timer__circle">
-//       <circle id="death" class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
-//     </g>
-//   </svg>
-//   <span id="base-timer-label" class="base-timer__label">${formatTime(timeLeft)}</span>
-// </div>
-// `;
-
-// startTimer();
-
-// function onTimesUp() {
-//   clearInterval(timerInterval);
-// }
-
-// function startTimer() {
-//   timerInterval = setInterval(() => {
-//     timePassed = timePassed += 1;
-//     timeLeft = TIME_LIMIT - timePassed;
-//     document.getElementById('base-timer-label').innerHTML = formatTime(
-//       timeLeft
-//     );
-
-//     if (timeLeft === 0) {
-//       onTimesUp();
-//       // window.location = './musab.html';
-//     }
-//   }, 1000);
-// }
-
-// let body = document.querySelector('body');
-
-// body.addEventListener('click', function resetTimer() {
-//   timePassed = 0;
-// });
-
-// function formatTime(time) {
-//   const minutes = Math.floor(time / 60);
-//   let seconds = time % 60;
-
-//   if (seconds < 10) {
-//     seconds = `0${seconds}`;
-//   }
-
-//   return `${minutes}:${seconds}`;
